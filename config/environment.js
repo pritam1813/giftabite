8c79b11ca98d02933f77717b8c44afc46d9eee56
Pritam Dhara <pritam1813@gmail.com>
2023-04-25 13:24:16 +0530

    Used Connect-mongo to store session cookie in db

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,8 @@ const development = {
     name: 'development',
     port: 3000,
     asset_path: 'assets/',
-    mongodb_uri: 'mongodb://127.0.0.1:27017/giftabite'
+    mongodb_uri: 'mongodb://127.0.0.1:27017/giftabite',
+    session_cookie_key: 'giftabitetestKey'
 };
 
 //For Production build
@@ -13,7 +14,8 @@ const production = {
     name: 'production',
     port: process.env.PORT,
     asset_path: 'public/',
-    mongodb_uri: process.env.MONGODB_URI
+    mongodb_uri: process.env.MONGODB_URI,
+    session_cookie_key: process.env.SESSION_COOKIE_KEY,
 };
 
 
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const env = require('./config/environment');            //Config file to set the
 const express = require('express');                     //Importing Express Module for creating APP
 const sassMiddleware = require('express-dart-sass');    //Middleware for compiling scss
 const app = express();                                  //Creating Express app
-require('./config/view_helper')(app);                   //For appending dynimic asset path to the views                                 //Creating the app
+require('./config/view_helper')(app);                   //For appending dynimic asset path to the views
 const path = require('path');                           //Module For referencing to path
 const db = require('./config/mongoose');                //Importing the Database
+const session = require('express-session');             //Library for creating Session and storing encrypted Session ID
+const MongoStore = require('connect-mongo');            //required fo storing the session cookie
 
 if (env.name === 'development') {
     //Syntax for using express dart sass
@@ -31,6 +33,20 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('view engine', 'pug');                          // Setting default View Engine(required to render html)
 app.set('views', './views');                            // Setting views folder to render the pages by view engine
 
+/* Cookie Sessions */
+//Defining session and cookie properties
+app.use(session({
+    name: 'Giftabite',
+    secret: env.session_cookie_key,                //used to encrypt the cookie
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        maxAge: (1000 * 60 * 100),              //cookie expires after this time
+    },
+    store: MongoStore.create({                  //Storing the session cookie in the database
+        mongoUrl: env.mongodb_uri       
+    })
+}));
 
 //Setting the App to use the routes folder to handle Different Routes
 app.use('/', require('./routes/index'));
